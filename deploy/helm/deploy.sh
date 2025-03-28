6308d79985325b01d1444f4e7072b3a3a9afad35
Yuval Turgeman <yturgema@redhat.com>
2025-03-28 13:14:05 -0400

    Add pgvector helmchart
    Signed-off-by: Yuval Turgeman <yturgema@redhat.com>

--- deploy/helm/deploy.sh
+++ deploy/helm/deploy.sh
@@ -4,10 +4,20 @@ read -r -p "Enter Hugging Face Token: " HF_TOKEN
 echo "$HF_TOKEN"
 
 NAMESPACE=llama-stack-rag
-
+PGVECTOR_PASSWORD="$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 10)"
+PGVECTOR_DBNAME=rag_blueprint
+ 
 oc new-project $NAMESPACE
 oc create secret -n $NAMESPACE generic huggingface-secret --from-literal=HF_TOKEN="$HF_TOKEN"
+oc create secret -n $NAMESPACE generic vectordb-app \
+       --from-literal=username=postgres \
+       --from-literal=password=$PGVECTOR_PASSWORD \
+       --from-literal=host=pgvector \
+       --from-literal=port=5432 \
+       --from-literal=dbname=$PGVECTOR_DBNAME
+
 oc annotate secret huggingface-secret -n $NAMESPACE meta.helm.sh/release-name=rag meta.helm.sh/release-namespace=$NAMESPACE
+oc annotate secret vectordb-app -n $NAMESPACE meta.helm.sh/release-name=rag meta.helm.sh/release-namespace=$NAMESPACE
 
 # DOMAIN=$(kubectl get Ingress.config.openshift.io/cluster -o jsonpath='{.spec.domain}')
 
